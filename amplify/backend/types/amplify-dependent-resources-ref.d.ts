2e337f8efcd4203ec957cdf712877e300762c42d
Christina Delancy <cdelancy11@yahoo.com>
2022-02-12 01:55:11 -0500

    Deploy

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "livechatpwa0424999b": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "CreatedSNSRole": "string"
+        }
+    },
+    "api": {
+        "livechatpwa": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import "./styles/globals.css";
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import Amplify from '@aws-amplify/core';
+import awsconfig from './aws-exports';
+Amplify.configure({ ...awsconfig, ssr: true });
+
+const federated = {
+  google_client_id: "517072930998-t95t9729vmgccsobl0qa313g6lqso7h6.apps.googleusercontent.com"
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App messages={[]} federated={federated} />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
+
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
